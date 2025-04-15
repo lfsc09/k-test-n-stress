56c734e47b95a4481141a911b42af100351037a7
Lucas Castro <lfsc09@gmail.com>
2025-04-15 11:26:25 -0300

    feat: mock cobra command that will handle creation of mock data

---
+++ cmd/mock.go
@@ -0,0 +1,169 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/lfsc09/k-test-n-stress/mock"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var mockCmd = &cobra.Command{
+	Use:   "mock",
+	Short: "Generate mock data based on requested function names in the values of the parsed json object",
+	Run: func(cmd *cobra.Command, args []string) {
+		saveTo := viper.GetBool("saveTo")
+		parseStr := viper.GetString("parse")
+		parseFromFiles := viper.GetStringSlice("parseFrom")
+
+		if parseStr == "" && len(parseFromFiles) == 0 {
+			log.Fatalln("Nothing to be parsed. Ask for help -h or --help")
+		} else if parseStr != "" && len(parseFromFiles) != 0 {
+			log.Fatalln("Please provide only one of the two options: --parse or --parseFrom")
+		}
+
+		// Parse single string object
+		if parseStr != "" {
+			var parseMap map[string]interface{}
+			if err := json.Unmarshal([]byte(parseStr), &parseMap); err != nil {
+				log.Fatalf("Opss..failed to parse JSON from the provided --parse <string>: %v\n", err)
+			}
+
+			mocker := mock.New()
+			if err := processMap(parseMap, mocker); err != nil {
+				log.Fatalln(err)
+			}
+
+			if err := processOutput(saveTo, "mocked-data.json", &parseMap); err != nil {
+				log.Fatalln(err)
+			}
+		}
+
+		if len(parseFromFiles) != 0 {
+			var wg sync.WaitGroup
+			for _, filename := range parseFromFiles {
+
+				wg.Add(1)
+				go func(filename string) {
+					defer wg.Done()
+
+					if !strings.HasSuffix(filename, ".template.json") {
+						log.Printf("File '%s' must have a .template.json extension\n", filename)
+						return
+					}
+
+					fileContent, err := os.ReadFile(filename)
+					if err != nil {
+						log.Printf("Opss..failed to read --parseFrom <file>: %v\n", err)
+						return
+					}
+
+					var parseMap map[string]interface{}
+					if err = json.Unmarshal(fileContent, &parseMap); err != nil {
+						log.Printf("Opss..failed to parse JSON from the provided --parseFrom <file>: %v\n", err)
+						return
+					}
+
+					mocker := mock.New()
+					if err := processMap(parseMap, mocker); err != nil {
+						log.Println(err)
+						return
+					}
+
+					outFilename := strings.Replace(filename, ".template.json", ".json", 1)
+					log.Printf("Parsing file %v -> %v\n", filename, outFilename)
+					if err := processOutput(saveTo, outFilename, &parseMap); err != nil {
+						log.Println(err)
+						return
+					}
+				}(filename)
+			}
+			wg.Wait()
+			log.Println("All files processed")
+		}
+	},
+}
+
+func init() {
+	mockCmd.Flags().String("saveTo", "", "Write mock data to '*.json' files")
+	mockCmd.Flags().String("parse", "", "Parse json object as string")
+	mockCmd.Flags().String("parseFrom", "", "Parse mock data from '.template.json' files")
+
+	viper.BindPFlag("saveTo", mockCmd.Flags().Lookup("saveTo"))
+	viper.BindPFlag("parse", mockCmd.Flags().Lookup("parse"))
+	viper.BindPFlag("parseFrom", mockCmd.Flags().Lookup("parseFrom"))
+
+	rootCmd.AddCommand(mockCmd)
+}
+
+/*
+The string type "value" interpreted from the JSON object can be:
+  - A mock function name (e.g. "Address.city")
+  - A mock function name with parameters (e.g. "Boolean.booleanWithChance:10")
+*/
+func interpretString(value string) (string, []string, error) {
+	explodedValue := strings.Split(value, ":")
+	if len(explodedValue) == 0 {
+		return "", nil, nil
+	} else if len(explodedValue) == 1 {
+		return explodedValue[0], nil, nil
+	} else {
+		return explodedValue[0], explodedValue[1:], nil
+	}
+}
+
+func processMap(parseMap map[string]interface{}, mocker *mock.Mock) error {
+	for key, value := range parseMap {
+		switch typedValue := value.(type) {
+		case string:
+			functionName, params, err := interpretString(typedValue)
+			if err != nil {
+				log.Fatalf("Ops..failed to interpret the value: %v", err)
+			}
+			parseMap[key] = mocker.Generate(functionName, params)
+		case map[string]interface{}:
+			err := processMap(typedValue, mocker)
+			if err != nil {
+				return err
+			}
+		default:
+			return fmt.Errorf("Value '%v' is not a string or map", value)
+		}
+	}
+	return nil
+}
+
+func processOutput(saveTo bool, filename string, result *map[string]interface{}) error {
+	if saveTo {
+		return toFile(filename, result)
+	} else {
+		return toStdout(result)
+	}
+}
+
+func toFile(filename string, result *map[string]interface{}) error {
+	prettyJSON, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return fmt.Errorf("Error marshalling JSON: %v", err)
+	}
+
+	err = os.WriteFile(filename, prettyJSON, 0644)
+	if err != nil {
+		return fmt.Errorf("Failed to write result to '%s': %v", filename, err)
+	}
+	return nil
+}
+
+func toStdout(result *map[string]interface{}) error {
+	prettyJSON, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return fmt.Errorf("Error marshalling JSON: %v", err)
+	}
+	fmt.Println(string(prettyJSON))
+	return nil
+}
