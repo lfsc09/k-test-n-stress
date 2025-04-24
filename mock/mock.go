789f053f4faf5436b7f1cf8f0b97c8a504c1ef1f
Lucas Castro <lfsc09@gmail.com>
2025-04-24 01:08:06 -0300

    feat: read bellow
    - Refactor for `Generate` to return error intead of `log.Fatal`.
    - New `cnpj` and `cpf` mockers.
    - New random `regex` mocker.

--- mock/mock.go
+++ mock/mock.go
@@ -1,7 +1,8 @@
 package mock
 
 import (
-	"log"
+	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 
@@ -25,174 +26,275 @@ func New() *Mock {
 	}
 }
 
-func (m *Mock) Generate(mockFunction string, functionParams []string) string {
+func (m *Mock) Generate(mockFunction string, functionParams []string) (string, error) {
 	switch mockFunction {
 	/*
 		ADDRESSES
 	*/
-	case "Address.address":
-		return m.faker.Address().Address()
-	case "Address.city":
-		return m.faker.Address().City()
-	case "Address.country":
-		return m.faker.Address().Country()
+	case "Address.latitude":
+		return strconv.FormatFloat(m.faker.Address().Latitude(), 'f', 6, 64), nil
+	case "Address.longitude":
+		return strconv.FormatFloat(m.faker.Address().Longitude(), 'f', 6, 64), nil
 	case "Address.postCode":
-		return m.faker.Address().PostCode()
+		return m.faker.Address().PostCode(), nil
+	case "Address.country":
+		return m.faker.Address().Country(), nil
 	case "Address.state":
-		return m.faker.Address().State()
-	case "Address.streetAddress":
-		return m.faker.Address().StreetAddress()
+		return m.faker.Address().State(), nil
+	case "Address.city":
+		return m.faker.Address().City(), nil
 	case "Address.streetName":
-		return m.faker.Address().StreetName()
+		return m.faker.Address().StreetName(), nil
+	case "Address.buildingNumber":
+		return m.faker.Address().BuildingNumber(), nil
 	/*
 		BOOLEANS
 	*/
 	case "Boolean.boolean":
-		return strconv.FormatBool(m.faker.Boolean().Bool())
+		return strconv.FormatBool(m.faker.Boolean().Bool()), nil
 	case "Boolean.booleanWithChance":
 		chance, err := strconv.Atoi(functionParams[0])
 		if err != nil {
-			return strconv.FormatBool(m.faker.Boolean().Bool())
+			return strconv.FormatBool(m.faker.Boolean().Bool()), nil
 		}
-		return strconv.FormatBool(m.faker.Boolean().BoolWithChance(chance))
+		return strconv.FormatBool(m.faker.Boolean().BoolWithChance(chance)), nil
 	/*
 		CAR
 	*/
 	case "Car.maker":
-		return m.faker.Car().Maker()
+		return m.faker.Car().Maker(), nil
 	case "Car.model":
-		return m.faker.Car().Model()
+		return m.faker.Car().Model(), nil
 	case "Car.plate":
-		return m.faker.Car().Plate()
+		return m.faker.Car().Plate(), nil
 	/*
 		COMPANY
 	*/
 	case "Company.name":
-		return m.faker.Company().Name()
+		return m.faker.Company().Name(), nil
 	case "Company.suffix":
-		return m.faker.Company().Suffix()
+		return m.faker.Company().Suffix(), nil
+	case "Company.catchPhrase":
+		return m.faker.Company().CatchPhrase(), nil
+	case "Company.bs":
+		return m.faker.Company().BS(), nil
+	case "Company.jobTitle":
+		return m.faker.Company().JobTitle(), nil
 	case "Company.cnpj":
-		cnpj, err := regen.Generate("[0-9]{2}.[0-9]{3}.[0-9]{3}/[0-9]{4}-[0-9]{2}")
-		if err != nil {
-			log.Fatalf("Failed to generate CNPJ: %v\n", err)
+		cnpj := make([]int, 12)
+
+		// Generate the first 12 random digits
+		for i := range 12 {
+			cnpj[i] = rand.Intn(10)
 		}
-		return cnpj
+
+		// Multipliers for checksum digits
+		multipliers1 := []int{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2}
+		multipliers2 := []int{6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2}
+
+		// Calculate checksums and append them
+		cnpj = append(cnpj, calculateChecksum(cnpj, multipliers1))
+		cnpj = append(cnpj, calculateChecksum(cnpj, multipliers2))
+
+		// Format the CNPJ
+		return fmt.Sprintf("%02d.%03d.%03d/%04d-%02d",
+			cnpj[0]*10+cnpj[1],
+			cnpj[2]*100+cnpj[3]*10+cnpj[4],
+			cnpj[5]*100+cnpj[6]*10+cnpj[7],
+			cnpj[8]*1000+cnpj[9]*100+cnpj[10]*10+cnpj[11],
+			cnpj[12]*10+cnpj[13],
+		), nil
 	/*
 		CURRENCY
 	*/
 	case "Currency.currencyCode":
-		return m.faker.Currency().Code()
+		return m.faker.Currency().Code(), nil
 	case "Currency.currencyContry":
-		return m.faker.Currency().Country()
+		return m.faker.Currency().Country(), nil
 	case "Currency.currencyName":
-		return m.faker.Currency().Currency()
+		return m.faker.Currency().Currency(), nil
 	case "Currency.currencyNumber":
-		return strconv.Itoa(m.faker.Currency().Number())
+		return strconv.Itoa(m.faker.Currency().Number()), nil
 	/*
 		FILE
 	*/
 	case "File.filenameWithExtension":
-		return m.faker.File().FilenameWithExtension()
+		return m.faker.File().FilenameWithExtension(), nil
 	case "File.extension":
-		return m.faker.File().Extension()
+		return m.faker.File().Extension(), nil
 	/*
 		INTERNET
 	*/
-	case "Internet.email":
-		return m.faker.Internet().Email()
 	case "Internet.domain":
-		return m.faker.Internet().Domain()
-	case "Internet.url":
-		return m.faker.Internet().URL()
+		return m.faker.Internet().Domain(), nil
+	case "Internet.email":
+		return m.faker.Internet().Email(), nil
 	case "Internet.ipv4":
-		return m.faker.Internet().Ipv4()
+		return m.faker.Internet().Ipv4(), nil
+	case "Internet.macAddress":
+		return m.faker.Internet().MacAddress(), nil
 	case "Internet.password":
-		return m.faker.Internet().Password()
+		return m.faker.Internet().Password(), nil
+	case "Internet.url":
+		return m.faker.Internet().URL(), nil
 	/*
 		LOREM
 	*/
 	case "Lorem.paragraph":
 		sentences, err := strconv.Atoi(functionParams[0])
 		if err != nil {
-			return m.faker.Lorem().Paragraph(1)
+			return m.faker.Lorem().Paragraph(1), nil
 		}
-		return m.faker.Lorem().Paragraph(sentences)
+		return m.faker.Lorem().Paragraph(sentences), nil
 	case "Lorem.paragraphs":
 		paragraphs, err := strconv.Atoi(functionParams[0])
 		if err != nil {
-			return strings.Join(m.faker.Lorem().Paragraphs(1), "")
+			return strings.Join(m.faker.Lorem().Paragraphs(1), ""), nil
 		}
-		return strings.Join(m.faker.Lorem().Paragraphs(paragraphs), "\n")
+		return strings.Join(m.faker.Lorem().Paragraphs(paragraphs), "\n"), nil
 	case "Lorem.sentence":
 		words, err := strconv.Atoi(functionParams[0])
 		if err != nil {
-			return m.faker.Lorem().Sentence(1)
+			return m.faker.Lorem().Sentence(1), nil
 		}
-		return m.faker.Lorem().Sentence(words)
+		return m.faker.Lorem().Sentence(words), nil
 	case "Lorem.sentences":
 		sentences, err := strconv.Atoi(functionParams[0])
 		if err != nil {
-			return strings.Join(m.faker.Lorem().Sentences(1), "")
+			return strings.Join(m.faker.Lorem().Sentences(1), ""), nil
 		}
-		return strings.Join(m.faker.Lorem().Sentences(sentences), "\n")
+		return strings.Join(m.faker.Lorem().Sentences(sentences), "\n"), nil
 	case "Lorem.word":
-		return m.faker.Lorem().Word()
+		return m.faker.Lorem().Word(), nil
 	case "Lorem.words":
 		words, err := strconv.Atoi(functionParams[0])
 		if err != nil {
-			return strings.Join(m.faker.Lorem().Words(1), " ")
+			return strings.Join(m.faker.Lorem().Words(1), " "), nil
 		}
-		return strings.Join(m.faker.Lorem().Words(words), "")
+		return strings.Join(m.faker.Lorem().Words(words), ""), nil
+	/*
+		NUMBER
+	*/
+	case "Number.number":
+		decimals := 0
+		min := -1000.0
+		max := 1000.0
+		if len(functionParams) > 0 && functionParams[0] != "" {
+			decimals, _ = strconv.Atoi(functionParams[0])
+		}
+		if len(functionParams) > 1 && functionParams[1] != "" {
+			min, _ = strconv.ParseFloat(functionParams[1], 64)
+		}
+		if len(functionParams) > 2 && functionParams[2] != "" {
+			max, _ = strconv.ParseFloat(functionParams[2], 64)
+		}
+		return strconv.FormatFloat(m.faker.Float64(decimals, int(min), int(max)), 'f', decimals, 64), nil
 	/*
 		PAYMENT
 	*/
 	case "Payment.creditCardExpirationDate":
-		return m.faker.Payment().CreditCardExpirationDateString()
+		return m.faker.Payment().CreditCardExpirationDateString(), nil
 	case "Payment.creditCardNumber":
-		return m.faker.Payment().CreditCardNumber()
+		return m.faker.Payment().CreditCardNumber(), nil
 	case "Payment.creditCardType":
-		return m.faker.Payment().CreditCardType()
+		return m.faker.Payment().CreditCardType(), nil
 	case "Payment.creditCardCvv":
 		cvv, err := regen.Generate("[0-9]{3}")
 		if err != nil {
-			log.Fatalf("Failed to generate CVV: %v\n", err)
+			return "", fmt.Errorf("Failed to generate CVV: %v\n", err)
 		}
-		return cvv
+		return cvv, nil
 	/*
 		PERSON
 	*/
 	case "Person.phoneNumber":
-		return m.faker.Person().Contact().Phone
+		return m.faker.Person().Contact().Phone, nil
 	case "Person.email":
-		return m.faker.Person().Contact().Email
+		return m.faker.Person().Contact().Email, nil
 	case "Person.firstName":
-		return m.faker.Person().FirstName()
+		return m.faker.Person().FirstName(), nil
 	case "Person.lastName":
-		return m.faker.Person().LastName()
-	case "Person.fullName":
-		return m.faker.Person().FirstName() + " " + m.faker.Person().LastName()
+		return m.faker.Person().LastName(), nil
+	case "Person.name":
+		return m.faker.Person().Name(), nil
 	case "Person.cpf":
-		cpf, err := regen.Generate("[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}")
-		if err != nil {
-			log.Fatalf("Failed to generate CPF: %v\n", err)
+		cpf := make([]int, 9)
+
+		// Generate the first 9 random digits
+		for i := range 9 {
+			cpf[i] = rand.Intn(10)
 		}
-		return cpf
+
+		// Multipliers for checksum digits
+		multipliers1 := []int{10, 9, 8, 7, 6, 5, 4, 3, 2}
+		multipliers2 := []int{11, 10, 9, 8, 7, 6, 5, 4, 3, 2}
+
+		// Calculate checksums and append them
+		cpf = append(cpf, calculateChecksum(cpf, multipliers1))
+		cpf = append(cpf, calculateChecksum(cpf, multipliers2))
+
+		// Format the CPF
+		return fmt.Sprintf("%03d.%03d.%03d-%02d",
+			cpf[0]*100+cpf[1]*10+cpf[2],
+			cpf[3]*100+cpf[4]*10+cpf[5],
+			cpf[6]*100+cpf[7]*10+cpf[8],
+			cpf[9]*10+cpf[10],
+		), nil
+	/*
+		REGEX
+	*/
+	case "Regex.regex":
+		if len(functionParams) == 0 {
+			return "", fmt.Errorf("Regex function requires a regex pattern as parameter")
+		}
+		regex, err := extractRegex(functionParams[0])
+		if err != nil {
+			return "", err
+		}
+		randomRegex, err := regen.Generate(regex)
+		if err != nil {
+			return "", fmt.Errorf("Failed to generate regex: %v\n", err)
+		}
+		return randomRegex, nil
 	/*
 		TIME
 	*/
-
+	case "Time.date":
+		return "", nil
 	/*
 		UUID
 	*/
 	case "UUID.uuidv4":
-		return m.faker.UUID().V4()
+		return m.faker.UUID().V4(), nil
 	/*
 		USER AGENT
 	*/
 	case "UserAgent.userAgent":
-		return m.faker.UserAgent().UserAgent()
+		return m.faker.UserAgent().UserAgent(), nil
 	default:
-		log.Fatalf("Unknown mock function: %v\n", mockFunction)
-		return ""
+		return "", fmt.Errorf("Unknown mock function: %v\n", mockFunction)
 	}
+}
+
+// Helper function to calculate checksum for CPF and CNPJ
+func calculateChecksum(digits []int, multipliers []int) int {
+	sum := 0
+	for i := range digits {
+		sum += digits[i] * multipliers[i]
+	}
+	remainder := sum % 11
+	if remainder < 2 {
+		return 0
+	}
+	return 11 - remainder
+}
+
+// Extracts raw regex string from /.../ and unescapes \/ → /
+func extractRegex(value string) (string, error) {
+	if !strings.HasPrefix(value, "/") || !strings.HasSuffix(value, "/") {
+		return "", fmt.Errorf("Value '%s' must be wrapped in /.../", value)
+	}
+	trimmed := value[1 : len(value)-1]
+	unescaped := strings.ReplaceAll(trimmed, `\/`, `/`)
+	return unescaped, nil
 }
